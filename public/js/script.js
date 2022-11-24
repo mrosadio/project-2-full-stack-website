33535ed41bbf501af4f79989774f3baa728f956d
Ezequiel Benitez <ezequielbv@gmail.com>
2022-11-24 16:17:47 +0100

    add styles to profile. No request email in login. Featur to keep only first part of the city name

--- public/js/script.js
+++ public/js/script.js
@@ -5,5 +5,12 @@ document.addEventListener("DOMContentLoaded", () => {
   for (let i = 0; i < cardsDate.length; i++) {
     cardsDate[i].innerText = cardsDate[i].innerText.slice(4, 15);
   }
+  //change the cities name to put only the first part before the ","
+  let citytitles = document.getElementsByClassName('card-title')
+  for (var i = citytitles.length - 1; i >= 0; i--) {
+    let cityName = citytitles[i].innerText.split(',')[0]
+    citytitles[i].innerText = cityName;
+  }
+
   console.log("project-2-full-stack-website JS imported successfully!");
 });
